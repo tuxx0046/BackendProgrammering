0862e8793367d389697d3961c5e87c342a1f72ed
tuxx0046 <tuxx0046@edu.sde.dk>
2021-02-08 14:38:16 +0100

    Started work on models and dataaccess

---
+++ BigShop/BigShop/BigShop.DataAccess/Db/ConnectionStringData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigShop.DataAccess.Db
+{
+    public class ConnectionStringData
+    {
+        public string SqlConnectionName { get; set; } = "Default";
+    }
+}
---
+++ BigShop/BigShop/BigShop.DataAccess/Db/IDataAccess.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigShop.DataAccess.Db
+{
+    public interface IDataAccess
+    {
+        Task<List<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionStringName);
+        Task<int> SaveData<T>(string storedProcedure, T parameters, string connectionStringName);
+    }
+}
