3a8d4987762a197d49c00dbe9b4b416be327bbee
tuxx0046 <tuxx0046@edu.sde.dk>
2021-02-08 15:56:32 +0100

    Account classes created
    + minor changes

--- BigShop/BigShop/BigShop.DataAccess/Db/SqlDb.cs
+++ BigShop/BigShop/BigShop.DataAccess/Db/SqlDb.cs
@@ -45,3 +45,4 @@ namespace BigShop.DataAccess.Db
             }
         }
     }
+}
---
+++ BigShop/BigShop/BigShop.Models/Account/ApplicationUserCreate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigShop.Models.Account
+{
+    public class ApplicationUserCreate : ApplicationUserLogin
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [MaxLength(30, ErrorMessage = "Must be at most 30 characters")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string Email { get; set; }
+    }
+}
