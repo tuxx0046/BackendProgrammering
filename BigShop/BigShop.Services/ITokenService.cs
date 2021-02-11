d442564f40211899aae1cb87f83581f06e42274c
tuxx0046 <tuxx0046@edu.sde.dk>
2021-02-11 21:09:37 +0100

    Created token services for login jwt functionality

---
+++ BigShop/BigShop.Services/ITokenService.cs
@@ -0,0 +1,14 @@
+﻿using BigShop.Models.Account;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigShop.Services
+{
+    public interface ITokenService
+    {
+        public string CreateToken(ApplicationUserIdentity user);
+    }
+}
