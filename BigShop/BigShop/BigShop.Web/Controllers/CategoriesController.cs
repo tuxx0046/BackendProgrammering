0b7e68214f73a9d04206b4406bde862d2fed15e3
tuxx0046 <tuxx0046@edu.sde.dk>
2021-02-12 14:54:47 +0100

    CategoriesController done

--- BigShop/BigShop/BigShop.Web/Controllers/CategoriesController.cs
+++ BigShop/BigShop/BigShop.Web/Controllers/CategoriesController.cs
@@ -1,4 +1,5 @@
 ﻿using BigShop.Models.Category;
+using BigShop.Models.Product;
 using BigShop.Repository;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -10,6 +11,7 @@ using System.Threading.Tasks;
 
 namespace BigShop.Web.Controllers
 {
+    //[Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class CategoriesController : ControllerBase
@@ -23,7 +25,6 @@ namespace BigShop.Web.Controllers
             _productRepository = productRepository;
         }
 
-        [Authorize]
         [HttpGet]
         [ProducesResponseType(200, Type = typeof(List<Category>))]
         [ProducesResponseType(400)]
@@ -37,13 +38,12 @@ namespace BigShop.Web.Controllers
             return Ok(categories);
         }
 
-        [Authorize]
-        [HttpGet("{id}", Name = "GetById")]
+        [HttpGet("{categoryId}", Name = "GetById")]
         [ProducesResponseType(200, Type = typeof(Category))]
         [ProducesResponseType(404)]
-        public async Task<ActionResult<Category>> GetById(int id)
+        public async Task<ActionResult<Category>> GetById(int categoryId)
         {
-            var category = await _categoryRepository.GetByIdAsync(id);
+            var category = await _categoryRepository.GetByIdAsync(categoryId);
             if (category == null)
             {
                 return NotFound("Category does not exist");
@@ -51,59 +51,78 @@ namespace BigShop.Web.Controllers
             return Ok(category);
         }
 
-        //[HttpPost]
-        //[ProducesResponseType(201, Type = typeof(Review))]
-        //[ProducesResponseType(400)]
-        //[ProducesResponseType(404)]
-        //[ProducesResponseType(500)]
-        //public async Task<ActionResult> Create([FromBody]CategoryCreate categoryCreate)
-        //{
-        //    if (reviewToCreate == null)
-        //        return BadRequest(ModelState);
+        [HttpPost]
+        [ProducesResponseType(201, Type = typeof(Category))]
+        [ProducesResponseType(400)]
+        public async Task<ActionResult> Create([FromBody] CategoryCreate categoryCreate)
+        {
+            if (ModelState.IsValid == false)
+                return StatusCode(400, ModelState);
 
-        //    if (!_reviewerRepository.ReviewerExists(reviewToCreate.Reviewer.Id))
-        //        ModelState.AddModelError("", "Reviewer doesn't exist!");
+            int categoryId = await _categoryRepository.CreateAsync(categoryCreate);
+            var newCategory = await _categoryRepository.GetByIdAsync(categoryId);
+            
+            return CreatedAtRoute("GetById", new { categoryId = newCategory.Id }, newCategory);
+        }
 
-        //    if (!_bookRepository.BookExists(reviewToCreate.Book.Id))
-        //        ModelState.AddModelError("", "Book doesn't exist!");
-
-        //    if (!ModelState.IsValid)
-        //        return StatusCode(404, ModelState);
-
-        //    reviewToCreate.Book = _bookRepository.GetBook(reviewToCreate.Book.Id);
-        //    reviewToCreate.Reviewer = _reviewerRepository.GetReviewer(reviewToCreate.Reviewer.Id);
-
-        //    if (!ModelState.IsValid)
-        //        return BadRequest(ModelState);
-
-        //    if (!_reviewRepository.CreateReview(reviewToCreate))
-        //    {
-        //        ModelState.AddModelError("", $"Something went wrong saving the review");
-        //        return StatusCode(500, ModelState);
-        //    }
-
-        //    return CreatedAtRoute("GetReview", new { reviewId = reviewToCreate.Id }, reviewToCreate);
-        //}
-
-        [Authorize]
-        [HttpDelete("{id}", Name = "Delete")]
+        [HttpDelete("{categoryId}", Name = "Delete")]
         [ProducesResponseType(204)] //no content
+        [ProducesResponseType(404)]
+        public async Task<ActionResult<int>> Delete(int categoryId)
+        {
+            var category = await _categoryRepository.GetByIdAsync(categoryId);
+            if (category == null)
+                return NotFound($"Category with Id {categoryId} does not exist");
+
+            int affectedrows = await _categoryRepository.DeleteAsync(categoryId);
+            return NoContent();
+
+        }
+
+        [HttpPut("{categoryId}")]
+        [ProducesResponseType(204)]
         [ProducesResponseType(400)]
         [ProducesResponseType(404)]
         [ProducesResponseType(500)]
-        public async Task<ActionResult<int>> Delete(int id)
+        public async Task<ActionResult<Category>> Update(int categoryId, [FromBody]Category updatedCategory)
         {
-            var category = await _categoryRepository.GetByIdAsync(id);
-            if (category == null)
-                return NotFound("Category does not exist");
-
-            var affectedrows = await _categoryRepository.DeleteAsync(id);
-            if (affectedrows > 0)
+            if (categoryId != updatedCategory.Id)
             {
-                return NoContent();
+                return BadRequest("Trying to edit category with other Id");
             }
 
-            return StatusCode(StatusCodes.Status500InternalServerError);
+            if (ModelState.IsValid == false)
+                return BadRequest(ModelState);
+
+            var oldCategory = await _categoryRepository.GetByIdAsync(categoryId);
+            if (oldCategory == null)
+            {
+                return NotFound($"Category with Id {categoryId} not found");
+            }
+
+            await _categoryRepository.UpdateAsync(updatedCategory);
+
+            return Ok();
         }
+
+        [HttpGet("{categoryId}/products")]
+        public async Task<ActionResult<List<Product>>> GetProductByCategoryId(int categoryId)
+        {
+            var categoryExist = await _categoryRepository.GetByIdAsync(categoryId);
+            if (categoryExist == null)
+            {
+                return NotFound($"Category with Id {categoryId} not found");
+            }
+            
+            var products = await _productRepository.GetByCategoryIdAsync(categoryId);
+            if (products.Count == 0)
+            {
+                return Ok($"No products in category \"{categoryExist.Name}\" found");
+            }
+
+            return Ok(products);
+
+        }
+
     }
 }
