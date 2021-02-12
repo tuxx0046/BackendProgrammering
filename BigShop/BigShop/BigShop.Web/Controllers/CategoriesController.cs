08bb544029a7ba9790266f2089f2a6b6369f5286
tuxx0046 <tuxx0046@edu.sde.dk>
2021-02-12 10:12:33 +0100

    Update CategoriesController.cs

--- BigShop/BigShop/BigShop.Web/Controllers/CategoriesController.cs
+++ BigShop/BigShop/BigShop.Web/Controllers/CategoriesController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BigShop.Models.Category;
+using BigShop.Repository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,5 +14,96 @@ namespace BigShop.Web.Controllers
     [ApiController]
     public class CategoriesController : ControllerBase
     {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IProductRepository _productRepository;
+
+        public CategoriesController(ICategoryRepository categoryRepository, IProductRepository productRepository)
+        {
+            _categoryRepository = categoryRepository;
+            _productRepository = productRepository;
+        }
+
+        [Authorize]
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(List<Category>))]
+        [ProducesResponseType(400)]
+        public async Task<ActionResult<List<Category>>> GetAll()
+        {
+            var categories = await _categoryRepository.GetAllAsync();
+            if (categories == null)
+            {
+                return BadRequest();
+            }
+            return Ok(categories);
+        }
+
+        [Authorize]
+        [HttpGet("{id}", Name = "GetById")]
+        [ProducesResponseType(200, Type = typeof(Category))]
+        [ProducesResponseType(404)]
+        public async Task<ActionResult<Category>> GetById(int id)
+        {
+            var category = await _categoryRepository.GetByIdAsync(id);
+            if (category == null)
+            {
+                return NotFound("Category does not exist");
+            }
+            return Ok(category);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(201, Type = typeof(Review))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        public async Task<ActionResult> Create([FromBody]CategoryCreate categoryCreate)
+        {
+            if (reviewToCreate == null)
+                return BadRequest(ModelState);
+
+            if (!_reviewerRepository.ReviewerExists(reviewToCreate.Reviewer.Id))
+                ModelState.AddModelError("", "Reviewer doesn't exist!");
+
+            if (!_bookRepository.BookExists(reviewToCreate.Book.Id))
+                ModelState.AddModelError("", "Book doesn't exist!");
+
+            if (!ModelState.IsValid)
+                return StatusCode(404, ModelState);
+
+            reviewToCreate.Book = _bookRepository.GetBook(reviewToCreate.Book.Id);
+            reviewToCreate.Reviewer = _reviewerRepository.GetReviewer(reviewToCreate.Reviewer.Id);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (!_reviewRepository.CreateReview(reviewToCreate))
+            {
+                ModelState.AddModelError("", $"Something went wrong saving the review");
+                return StatusCode(500, ModelState);
+            }
+
+            return CreatedAtRoute("GetReview", new { reviewId = reviewToCreate.Id }, reviewToCreate);
+        }
+
+        [Authorize]
+        [HttpDelete("{id}", Name = "Delete")]
+        [ProducesResponseType(204)] //no content
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        public async Task<ActionResult<int>> Delete(int id)
+        {
+            var category = await _categoryRepository.GetByIdAsync(id);
+            if (category == null)
+                return NotFound("Category does not exist");
+
+            var affectedrows = await _categoryRepository.DeleteAsync(id);
+            if (affectedrows > 0)
+            {
+                return NoContent();
+            }
+
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
     }
 }
